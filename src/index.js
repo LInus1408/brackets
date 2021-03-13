083ee275295367f56566bc95a8d8bf36670f6f35
Maksim Hasanau <maxlinus1408@gmail.com>
2021-03-13 21:12:23 +0300

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let abc = false;
+  while (str.length != 0 && !abc) {
+    abc = true;
+      for (let check1 of bracketsConfig) {
+        let check2 = check1.join('');
+        if (str.includes(check2)) {
+        str = str.replace(check2, '');
+        abc = false;
+      }
+    }
+  } 
+  return str.length == false; 
 }
